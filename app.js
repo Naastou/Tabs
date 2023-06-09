c33c0247688405f243606affe0030fddad3f5f2f
Na Astou <naastou1@gmail.com>
2023-06-09 11:57:57 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const btnTabs = document.querySelectorAll('.btn');
+const tabContents= document.querySelectorAll('.text');
+
+btnTabs.forEach(tab => {
+  tab.addEventListener('click', (e) => {
+   
+    const activeTabId = e.currentTarget.dataset.id;
+  
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    });
+    btnTabs.forEach(tab => {
+      tab.classList.remove('active')
+    });
+    const element = document.getElementById(activeTabId);
+    element.classList.add('active');
+  });
+});
+
