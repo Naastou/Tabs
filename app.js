e083592edabd2222e4b031008b9ed60aa640fd27
Na Astou <naastou1@gmail.com>
2023-06-09 12:04:46 +0200

    modified app

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ btnTabs.forEach(tab => {
   
     tabContents.forEach(tabContent => {
       tabContent.classList.remove('active')
-    });
+    })
     btnTabs.forEach(tab => {
       tab.classList.remove('active')
-    });
+    })
     const element = document.getElementById(activeTabId);
     element.classList.add('active');
   });
-});
+})
 
